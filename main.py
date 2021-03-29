5422c73d70c65dcea40d7015cb5316331e0c5b65
roduquen <duquenne.robin42@gmail.com>
2021-03-29 18:40:56 +0200

    binance_key protected in gitignore

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from client.websocket_client import WebSocketClient
 from client.http_client import HTTPClient
 from array_class.array import Array
-
+from binance_keys import *
 
 http_client = HTTPClient(api_key_id, secret_key)
 websocket_client = WebSocketClient(api_key_id, secret_key)
